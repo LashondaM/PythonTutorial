9d2f85f7b66cbc9bc656818ec0e5e8db1e08c8c5
Lashonda Miller <lashondamiller007@gmail.com>
2023-11-17 11:15:11 -0500

    More python lessons

---
+++ Lesson01/hello.py
@@ -0,0 +1,4 @@
+greeting = 'Hello World!'
+print(greeting)
+
+# you can also type py hello.py in the terminal to run this file. Make sure the Path is shwing the folder that the file is in in order to run this command
